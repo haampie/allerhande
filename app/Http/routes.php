a6c9c7c46693b2708b9ccc9fa28c65ee8d7970c9
Harmen <harmenstoppels@gmail.com>
2016-08-14 01:44:11 +0200

    Cosmetics

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-$app->get('/', function () use ($app) {
+$app->get('/recipe', function () use ($app) {
     $starts = ['Steek de', 'Rijg de', 'Snijd de', 'Laat de', 'Neem de', 'Roer de'];
     $start = $starts[array_rand($starts)];
     return response($app['generator']($start, 100));
