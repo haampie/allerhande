39ec3538706b844ec3a5d86af9429302b891bec3
Harmen <harmenstoppels@gmail.com>
2016-08-14 02:19:46 +0200

    Diversity

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,19 @@
 <?php
 
 $app->get('/recipe', function () use ($app) {
-    $starts = ['Steek de', 'Rijg de', 'Snijd de', 'Laat de', 'Neem de', 'Roer de'];
+    $starts = [
+        'Steek de',
+        'Rijg de',
+        'Snijd de',
+        'Laat de',
+        'Neem de',
+        'Roer de',
+        'Was de',
+        'Verhit de',
+        'Breng een',
+        'Boen de',
+        'Meng de',
+    ];
     $start = $starts[array_rand($starts)];
     return response($app['generator']($start, 100));
 });
